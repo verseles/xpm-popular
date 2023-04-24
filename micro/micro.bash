c1e7f7a25aa0b3399a646e3998eefde9fb74f1d7
Hélio <insign@gmail.com>
2023-04-23 22:05:39 -0300

    fix zypper micro package name

--- micro/micro.bash
+++ micro/micro.bash
@@ -11,19 +11,20 @@ readonly xARCH=('linux64' 'linux32' 'linux-arm' 'linux-arm64' 'macos-arm64' 'mac
 readonly xLICENSE="https://raw.githubusercontent.com/zyedidia/micro/v$xVERSION/LICENSE"
 readonly xPROVIDES=("micro")
 
-# variables which is dinamically set
+# variables which is dinamically set and available for use
 # $yCHANNEL
 #  the default channel is empty, which means the latest stable version
 #  user can change using -c or --channel flag
 # $isSnap, $isFlatpack, $isAppimage
-#  only when using snap, flatpack or appimage, these variables are available as boolean
+#  these boolean variables are set to true if the package manager is available and selected
 # $XPM is the path to xpm executable
 # $ySUDO is the sudo command, if available. Most commands already add sudo if available
 
-# optional methods install_apt, remove_apt, install_pacman, remove_pacman, install_dnf, remove_dnf, install_pack, remove_pack, install_yum, remove_yum, install_choco, remove_choco, install_brew, remove_brew, install_zypper, remove_zypper, install_android, remove_android, validate
+# the only required function is validate. install_any and remove_any are very important, but not required.
 validate() { # $1 is the path to executable from $xPROVIDES (if defined) or $xNAME
 	$1 --version
 }
+
 install_any() {
 	# shellcheck disable=SC1090
 	sh "$($XPM get https://getmic.ro --exec --no-progress)"
@@ -54,17 +55,19 @@ install_dnf() {    # $1 means an executable compatible with dnf (Fedora)
 	$1 install $xNAME # with -y, with sudo if available
 }
 
-remove_dnf() { # $1 means dnf compatible with -y, with sudo if available
-	$1 remove -y $xNAME
+remove_dnf() {       # $1 means dnf compatible with -y, with sudo if available
+	$1 remove -y $xNAME # with -y, with sudo if available
 }
 
 install_pack() { # $1 means an executable compatible with snap, flatpack or appimage
 	# $isSnap, $isFlatpack, $isAppimage are available as boolean
 	# shellcheck disable=SC2154
 	if [[ $isFlatpack == true ]]; then   # actually micro is not available on flatpack
-		$1 install $xNAME                   # with --assumeyes
+		return 1
+		# $1 install $xNAME                   # with --assumeyes
 	elif [[ $isAppimage == true ]]; then # actually micro is not available on appimage
-		$1 install $xNAME
+		return 1
+		# $1 install $xNAME
 	else
 		$1 install $xNAME
 	fi
@@ -82,8 +85,8 @@ remove_pack() {
 	fi
 }
 
-install_choco() {  # $1 means an executable compatible  with chocolatey (Windows)
-	$1 install $xNAME # with -y
+install_choco() { # $1 means an executable compatible  with chocolatey (Windows)
+	$1 install $xNAME
 }
 
 remove_choco() { # $1 means choco compatible with -y
@@ -99,17 +102,27 @@ remove_brew() {
 }
 
 install_zypper() { # $1 means an executable compatible with zypper (openSUSE) or zypper with -y
-	$1 install $xNAME
+	$1 install $xNAME-editor # with --non-interactive, with sudo if available
 }
 
 remove_zypper() { # $1 means zypper compatible with -y
-	$1 remove -y $xNAME
+	$1 remove $xNAME-editor # with --non-interactive, with sudo if available
 }
 
 install_android() { # $1 means an executable compatible with pkg (Termux Android) with -y
-	$1 install $xNAME
+	$1 install $xNAME # with -y, with sudo if available
 }
 
 remove_android() { # $1 means pkg compatible with -y
-	$1 remove $xNAME
+	$1 remove $xNAME  # with -y, with sudo if available
+}
+
+install_swupd() { # $1 means an executable compatible with swupd (Clear Linux), with -y, with sudo if available
+	$1 bundle-add go-basic # we don't really need go, but it's just an example
+	install_any
+}
+
+remove_swupd() { # $1 means swupd compatible with -y, with sudo if available
+	$1 bundle-remove --orphans
+	remove_any
 }
