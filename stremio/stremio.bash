94e0fae4464a742763f50146423f26d0a9b5fd95
Hélio <insign@gmail.com>
2023-05-21 02:33:19 -0300

    testing stremio installer script on opensuse

--- stremio/stremio.bash
+++ stremio/stremio.bash
@@ -91,7 +91,7 @@ remove_swupd() {
 }
 
 install_zypper() {
-    $xSUDO $1 install git nodejs20 mpv wget libqt5-qtbase-devel libqt5-qtwebengine-devel \
+    $xSUDO $1 install git nodejs20 mpv-devel wget libqt5-qtbase-devel libqt5-qtwebengine-devel \
         libqt5-qtquickcontrols libopenssl-devel gcc gcc-c++ make glibc-devel kernel-devel binutils ||
         echo "zypper says some packages are already installed. Proceeding..."
 
