2febf0826ca7d352ba5902f80826a4c824906ff5
Hélio <insign@gmail.com>
2023-05-27 23:42:40 -0300

    chore(7zip.bash): update xVERSION from 22.01 to 23.00 fix(7zip.bash): fix typo in xPROVIDES array, change '7zzs' to '7zz' refactor(7zip.bash): rename install_pacman function to install_any and remove unused variables and code

--- 7zip/7zip.bash
+++ 7zip/7zip.bash
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
-# USE THIS FILE AS TEMPLATE FOR YOUR SCRIPT
+# shellcheck disable=SC2034 disable=SC2154 disable=SC2164 disable=SC2103
 
 readonly xNAME="7zip"
-readonly xVERSION="22.01"
+readonly xVERSION="23.00"
 readonly xTITLE="7-Zip"
 readonly xDESC="A free and open-source file archiver utility for compressing and uncompressing files"
 readonly xURL="https://www.7-zip.org"
 readonly xARCHS=('linux64' 'linux32' 'linux-arm' 'linux-arm64' 'macos-arm64' 'macos' 'win32' 'win64' 'freebsd64' 'freebsd32' 'openbsd64' 'openbsd32' 'netbsd64' 'netbsd32')
-readonly xLICENSE="LGPLv2"
-readonly xPROVIDES=('7z' '7za' '7zr')
+readonly xLICENSE="LGPL"
+readonly xPROVIDES=('7zzs' '7zz' '7za' '7zr')
 # The list of functions that use the default name (xNAME) on the package manager (for batch install)
 readonly xDEFAULT=()
 
@@ -20,14 +19,20 @@ validate() { # $1 is the path to executable from $xPROVIDES (if defined) or $xNA
 	$1
 }
 
-install_pacman() {
-	if $1 -Qs p7zip >/dev/null 2>&1; then
-		$XPM log warning "Removing p7zip package as it conflicts with 7-zip-full"
-		$1 -R p7zip
-	fi
-	$1 -S 7-zip-full
-}
+install_any() {
 
-remove_pacman() {
-	$1 -R 7-zip-full
+	echo $xARCH
+	exit 1
+	local OS
+	local VERSION
+	local ARCH
+	VERSION=${xVERSION//./}
+	if [[ $xOS == "macos" ]]; then
+		OS="macos"
+		ARCH=""
+	else
+		OS="$xOS"
+		ARCH="$xARCH"
+	fi
+	local binary="https://7-zip.org/a/7z$VERSION-$OS$ARCH.tar.xz"
 }
