2e340a8627ca27e87bb51300052e0e7786a22bbd
helio <open@helio.me>
2025-05-06 17:39:17 -0300

    refactor: tornar script mais compatível com POSIX

--- base.bash
+++ base.bash
@@ -1,7 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# Enable immediate output on error. This is a POSIX option.
 set -o errexit
+# Treat undefined variables as an error. This is a POSIX option.
 set -o nounset
-set -o pipefail
-if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
+
+# Check if the script is being run with BASH.
+if [ -n "$BASH_VERSION" ]; then
+    # Enable pipefail only if we're in bash, as it's not POSIX.
+    # pipefail makes the exit status of a pipeline the same as the last command to fail.
+    set -o pipefail
+fi
+
+# Enable debug mode (xtrace) if the TRACE environment variable is set to "1".
+if [ "${TRACE:-0}" = "1" ]; then
+    set -o xtrace # 'set -x' is a shorter equivalent and also POSIX.
 fi
